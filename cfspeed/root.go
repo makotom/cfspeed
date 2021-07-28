a7e0e84379344951095bb2f06fe5f5bdd515c9bb
Makoto Mizukami <comm@makotom.org>
2021-07-29 03:20:02 +0900

    Refactoring

--- cfspeed/root.go
+++ cfspeed/root.go
@@ -15,20 +15,22 @@ func printMetadata(metadata *MeasurementMetadata, err error) {
 		fmt.Fprintf(os.Stderr, "Error while fetching metadata: %v\n", err)
 		os.Exit(1)
 	} else if metadata != nil {
-		fmt.Printf("SrcIP: %s (AS%s)\n", metadata.srcIP, metadata.srcASN)
-		fmt.Printf("SrcLocation: %s, %s\n", metadata.srcCity, metadata.srcCountry)
-		fmt.Printf("DstColocation: %s\n", metadata.dstColo)
+		fmt.Printf("SrcIP: %s (AS%s)\n", metadata.SrcIP, metadata.SrcASN)
+		fmt.Printf("SrcLocation: %s, %s\n", metadata.SrcCity, metadata.SrcCountry)
+		fmt.Printf("DstColocation: %s\n", metadata.DstColo)
 	}
 }
 
-func printRTTMeasurement(measurement *RTTStats, err error) {
+func printRTTMeasurement(measurement *Stats, err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "RTT - Error during measurement: %v\n", err)
 		os.Exit(1)
 	} else if measurement != nil {
-		fmt.Printf("RTT-mean: %.3f ms\n", measurement.mean)
-		fmt.Printf("RTT-stderr: %.3f ms\n", measurement.stderr)
-		fmt.Printf("RTT-N: %d\n", measurement.nSamples)
+		fmt.Printf("RTT-mean: %.3f ms\n", measurement.Mean)
+		fmt.Printf("RTT-stderr: %.3f ms\n", measurement.StdErr)
+		fmt.Printf("RTT-min: %.3f ms\n", measurement.Min)
+		fmt.Printf("RTT-max: %.3f ms\n", measurement.Max)
+		fmt.Printf("RTT-n: %d\n", measurement.NSamples)
 	}
 }
 
@@ -37,10 +39,12 @@ func printAdaptiveSpeedMeasurement(label string, measurement *SpeedMeasurementSt
 		fmt.Fprintf(os.Stderr, "%s - Error during measurement: %v\n", label, err)
 		os.Exit(1)
 	} else if measurement != nil {
-		fmt.Printf("%s-mean: %.3f Mbps\n", label, measurement.mean)
-		fmt.Printf("%s-stderr: %.3f Mbps\n", label, measurement.stderr)
-		fmt.Printf("%s-tx: %.3f MiB\n", label, math.Round(float64(measurement.txSize)/1024/1024))
-		fmt.Printf("%s-N: %d\n", label, measurement.nSamples)
+		fmt.Printf("%s-mean: %.3f Mbps\n", label, measurement.Mean)
+		fmt.Printf("%s-stderr: %.3f Mbps\n", label, measurement.StdErr)
+		fmt.Printf("%s-min: %.3f Mbps\n", label, measurement.Min)
+		fmt.Printf("%s-max: %.3f Mbps\n", label, measurement.Max)
+		fmt.Printf("%s-tx: %.3f MiB\n", label, math.Round(float64(measurement.TXSize)/1024/1024))
+		fmt.Printf("%s-n: %d\n", label, measurement.NSamples)
 	}
 }
 
