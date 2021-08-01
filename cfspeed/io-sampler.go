4f692bda9b8916841f65b30f73ae5d5f111b45a7
Makoto Mizukami <comm@makotom.org>
2021-08-01 20:43:56 +0900

    Formatting

--- cfspeed/io-sampler.go
+++ cfspeed/io-sampler.go
@@ -25,15 +25,8 @@ type ReadSampler struct {
 	cEOF int64
 }
 
-func InitReadSampler(size int64) *ReadSampler {
-	r := &ReadSampler{}
-
-	r.IOSampler.Mode = "read"
-	r.IOSampler.CallEvents = []*IOEvent{}
-	r.ctr = 0
-	r.cEOF = size
-
-	return r
+type WriteSampler struct {
+	IOSampler
 }
 
 func (r *ReadSampler) Read(p []byte) (int, error) {
@@ -63,19 +56,6 @@ func (r *ReadSampler) Read(p []byte) (int, error) {
 	return size, err
 }
 
-type WriteSampler struct {
-	IOSampler
-}
-
-func InitWriteSampler() *WriteSampler {
-	w := &WriteSampler{}
-
-	w.IOSampler.Mode = "write"
-	w.IOSampler.CallEvents = []*IOEvent{}
-
-	return w
-}
-
 func (w *WriteSampler) Write(p []byte) (int, error) {
 	size := len(p)
 
@@ -86,4 +66,24 @@ func (w *WriteSampler) Write(p []byte) (int, error) {
 	})
 
 	return size, nil
+}
+
+func InitReadSampler(size int64) *ReadSampler {
+	r := &ReadSampler{}
+
+	r.IOSampler.Mode = "read"
+	r.IOSampler.CallEvents = []*IOEvent{}
+	r.ctr = 0
+	r.cEOF = size
+
+	return r
+}
+
+func InitWriteSampler() *WriteSampler {
+	w := &WriteSampler{}
+
+	w.IOSampler.Mode = "write"
+	w.IOSampler.CallEvents = []*IOEvent{}
+
+	return w
 }
